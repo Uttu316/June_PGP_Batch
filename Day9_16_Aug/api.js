157c276d5d9c781b418c2465ccb2f659e323b861
uttu-316 <utkarsh.gupta1@housing.com>
2023-08-16 23:52:41 +0530

    Day9

--- Day9_16_Aug/api.js
+++ Day9_16_Aug/api.js
@@ -2,8 +2,13 @@
 
 
 
-fetch('https://reqres.in/api/users').then((res)=>{
-    console.log(res)
+fetch('https://reqres.in/api/users').then((res)=>res.json()).then((res)=>{
+   const users = res.data
+   users.forEach(element => {
+        const p = document.createElement('p')
+        p.innerHTML = element.first_name+ " "+element.last_name
+        document.body.append(p)
+   });
 }).catch((err)=>{
     console.log(err)
 })
